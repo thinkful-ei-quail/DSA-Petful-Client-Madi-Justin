307ad724b39ca8458a1fffa9865b1f33ac14acc3
Madison Brown <59658823+mbcodes29@users.noreply.github.com>
2020-10-02 13:55:22 -0400

    Fixed page paths

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch} from 'react-router-dom';
 import './App.css'
-import LandingPage from '../routes/LandingPage';
-import AdoptionPage from '../routes/AdoptionPage';
+import LandingPage from '../routes/LandingPage/LandingPage';
+import AdoptionPage from '../routes/AdoptionPage/AdoptionPage';
 import AppContext from '../contexts/contexts'
 import config from '../../config';
 
